24c972ce6222a260ddbb8784da130643092d2824
PedritoBF03 <pedrobf2003@gmail.com>
2022-11-16 20:28:45 +0100

    Añadido de viajes, arreglado de clientes

--- backend/nievabus/src/app.module.ts
+++ backend/nievabus/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ClientesModule } from './modules/clientes/clientes.module';
 import { AutobusesModule } from './modules/autobuses/autobuses.module';
 import { EmpleadosModule } from './modules/empleados/empleados.module';
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { ViajesModule } from './modules/viajes/viajes.module';
 
 @Module({
-  imports: [ClientesModule, AutobusesModule, EmpleadosModule],
+  imports: [AutobusesModule, EmpleadosModule, ClientesModule, ViajesModule],
   controllers: [],
   providers: [],
 })
