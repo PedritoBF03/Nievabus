bb644df14c3945e91f530894f98cc3f20f85fa7e
PedritoBF03 <pedrobf2003@gmail.com>
2023-03-02 22:11:02 +0100

    Iniciando la APP

--- app-nievabus/pages/_document.tsx
+++ app-nievabus/pages/_document.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document'
+
+
+
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <title>Página de Nievabus</title>
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
