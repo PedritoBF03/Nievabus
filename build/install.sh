a0b1b32905ebbabf424f2ca40a9ad529587f34e0
PedritoBF03 <pedrobf2003@gmail.com>
2023-03-20 20:24:36 +0100

    actualizar

---
+++ build/install.sh
@@ -0,0 +1,23 @@
+
+echo "Desplegando la app de ptienda"
+
+# docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml build
+# docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml up -d
+
+cd /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus/
+
+export $(cat .env | xargs)
+
+docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus/docker-compose.yml build --no-cache
+
+docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus/docker-compose.yml up -d
+
+# docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/app_nievabus/docker-compose.yml build --no-cache
+
+# docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/app_nievabus/docker-compose.yml up -d
+
+
+# docker-compose -f ptienda/build/docker-compose.yml build --no-cache
+# docker-compose -f ptienda/build/docker-compose.yml down
+# docker-compose -f ptienda/build/docker-compose.yml up --remove-orphans
+
