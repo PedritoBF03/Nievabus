187cd3ec02943d81822d81961e191c9d0cbf7480
PedritoBF03 <pedrobf2003@gmail.com>
2023-03-20 20:30:01 +0100

    actualizar

--- build/install.sh
+++ build/install.sh
@@ -6,7 +6,8 @@ echo "Desplegando la app de ptienda"
 
 cd /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api-nievabus
 
-export $(cat .env | xargs)
+# export $(cat .env | xargs)
+export $(cat .env)
 
 docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api-nievabus/docker-compose.yml build --no-cache
 
