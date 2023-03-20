8fedc490327974e8da8815cc891d9ae57b07f1d9
PedritoBF03 <pedrobf2003@gmail.com>
2023-03-20 20:27:21 +0100

    actualizar

--- build/install.sh
+++ build/install.sh
@@ -4,13 +4,13 @@ echo "Desplegando la app de ptienda"
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml build
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml up -d
 
-cd /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus
+cd /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api-nievabus
 
 export $(cat .env | xargs)
 
-docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus/docker-compose.yml build --no-cache
+docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api-nievabus/docker-compose.yml build --no-cache
 
-docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus/docker-compose.yml up -d
+docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api-nievabus/docker-compose.yml up -d
 
 # docker-compose -f /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/app_nievabus/docker-compose.yml build --no-cache
 
