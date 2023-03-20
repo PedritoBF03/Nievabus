d3e9c440d6c8fddf4978338f98139fff533d1a07
PedritoBF03 <pedrobf2003@gmail.com>
2023-03-20 20:26:22 +0100

    actualizar

--- build/install.sh
+++ build/install.sh
@@ -4,7 +4,7 @@ echo "Desplegando la app de ptienda"
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml build
 # docker-compose -f /var/lib/jenkins/workspace/Proyectos/Job-pTienda/ptienda/build/docker-compose.yml up -d
 
-cd /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus/
+cd /var/lib/jenkins/workspace/proyecto_final/Job-pNievabus/api_nievabus
 
 export $(cat .env | xargs)
 
